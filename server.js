e083c8c7805f8480e1481ff38e34922e61006974
Umair Choudhary <umair.choudhary66@gmail.com>
2017-05-17 20:35:22 -0400

    changes in interface

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ var middleware = {
 
 app.use(middleware.logger);
 
-app.get('/', function(req, res){
-	res.send('Hey dude');
-});
-
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
